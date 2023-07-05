525bda2e2ceb416bc0578954f7b5e4fddba9cbb4
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-05 13:32:54 +0800

    Pseudocode for Pet class

---
+++ pet_class/pet_class.py
@@ -0,0 +1,13 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# creating a pet class which contains the following 
+# data attributes and methods required for the Assignment #9.
+
+# create a class named Pet
+# create the constructors, following attributes are pet's name, animal type, and its age
+# create a method that sets the name of the pet
+# create a method that sets the pet's animal type
+# create a method that sets the pet's age
+# create a method that gets the name of the pet
+# create a method that gets the pet's animal type
+# create a method that gets the pet's age
