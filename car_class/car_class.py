9c68b5ea427850cee1a40927398d466bda037c6b
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-05 15:57:19 +0800

    Pseudocode for Car class

---
+++ car_class/car_class.py
@@ -0,0 +1,10 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# Creating a car class that contains attributes and methods 
+# required by the Assignment # 9
+
+# create a class named car
+# create a constructor containing the car's year model, make, and its current speed
+# create a method that will accelerate the car
+# create a method that can trigger the car's brake
+# create a method that gets the car's current speed
