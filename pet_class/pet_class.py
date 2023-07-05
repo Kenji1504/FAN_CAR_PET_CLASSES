4a2aed66143d5b3a6921b84612eda5b903b0b9c2
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-05 13:41:29 +0800

    creating constructor containing private attributes

--- pet_class/pet_class.py
+++ pet_class/pet_class.py
@@ -3,8 +3,13 @@
 # creating a pet class which contains the following 
 # data attributes and methods required for the Assignment #9.
 
-# create a class named Pet
-# create the constructors, following attributes are pet's name, animal type, and its age
+#create a class named "Pet"
+class Pet:
+    # create the constructors, following attributes are pet's name, animal type, and its age
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
 # create a method that sets the name of the pet
 # create a method that sets the pet's animal type
 # create a method that sets the pet's age
