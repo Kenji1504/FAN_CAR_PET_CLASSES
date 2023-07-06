8101e5ffaa54cc6df7289a18b8627fbfb11cd2a5
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-06 20:28:26 +0800

    Pseudocode for Fan class

---
+++ fan_class/fan_class.py
@@ -0,0 +1,16 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# Creating a Fan class containing the following attributes and 
+# methods required by Assignment #9.
+
+# create a class named 'Fan'
+# create a constructor, contains the fan's data attributes 
+# such as speed, power indicator, radius, and color
+# create a method that access the fan's speed
+# create a method that access the fan's power indicator
+# create a method that access the fan's radius
+# create a method that access the fan's color
+# create a method that will set the fan's speed
+# create a method that will set the fan's power indicator
+# create a method that will set the fan's radius
+# create a method that will set the fan's color
