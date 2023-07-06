0f228547a4f400974f295e4d36f64a9ac47d76bd
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-06 20:43:10 +0800

    creating constructor

--- fan_class/fan_class.py
+++ fan_class/fan_class.py
@@ -3,7 +3,16 @@
 # Creating a Fan class containing the following attributes and 
 # methods required by Assignment #9.
 
+SLOW = 1
+MEDIUM = 2
+FAST = 3 
 # create a class named 'Fan'
+class Fan:
+    def __init__(self, speed= SLOW, power_indicator= False, radius= 5.0, color= "Blue"):
+        self.__speed = speed
+        self.__power_indicator = power_indicator
+        self.__radius = radius
+        self.__color = color
 # create a constructor, contains the fan's data attributes 
 # such as speed, power indicator, radius, and color
 # create a method that access the fan's speed
